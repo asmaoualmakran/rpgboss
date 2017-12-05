6f68cfdbc6ca6f8482fe23749486535a9c68d3db
Asma Oualmakran <asmaoualmakran@gmail.com>
2017-12-05 11:18:15 +0100

    Added: isLeaf boolean, ssceleton balancing tree. Bug fixes

--- desktop/src/main/scala/rpgboss/editor/BST/BinarySearchTree.scala
+++ desktop/src/main/scala/rpgboss/editor/BST/BinarySearchTree.scala
@@ -1,5 +1,12 @@
 package rpgboss.editor.BST
 
+/*
+  Author: Asma Oualmakran
+  Class: BinarySearchTree
+  Parameters: size:Int
+    use: Determine the size (number of nodes in the tree)
+  Use: Create a unordered binary search tree
+ */
 
 /*
   Every node of the bst has type T
@@ -16,9 +23,13 @@ class BinarySearchTree[T](size: Int) extends Tnode[T]{
     first node is added
    */
 
-  private def initTree(node:Node[T]): Unit ={
+  private def initTree(node:Node[T]): Unit={
     treeArray(rootIndex) = node
   }
+
+  /*
+    Get the root of the tree
+   */
 
   def getRoot(): Node[T]={
 
@@ -30,26 +41,33 @@ class BinarySearchTree[T](size: Int) extends Tnode[T]{
     }
   }
 
+  /*
+    Determine if a node is a leaf
+   */
+  
+  private def isLeaf_?(node: Node[T]): Boolean={
+    val left = node.leftChild
+    val right = node.rightChild
+
+    return left == null && right == null
+  }
+
+  /*
+    Balance the tree after adding a node
+   */
+
+  private def balanceTree(): Unit={
+
+    println("Tree is balanced")
+
+  }
+
 
   /*
     The node passed as pm, is not always the last node in the TreeSet
    */
   override def setChild(node:Node[T],newChildNode:Node[T]): Unit ={
 
-  /*  val newParentNode = findFreeLocation()
-
-    if(newParentNode == null){
-      addNode(childNode)   // if the tree is empty
-
-    }else if(newParentNode.leftChild == null){
-      newParentNode.leftChild = childNode
-      childNode.parent = newParentNode
-
-    }else if(newParentNode.rightChild == null){
-      newParentNode.rightChild = childNode
-      childNode.parent = newParentNode
-
-    } */
     if(getRoot() == null){
       initTree(newChildNode)     // in this case the tree has not been initialized yet with a root
                               // we add te new node, and set is as root
@@ -78,25 +96,46 @@ class BinarySearchTree[T](size: Int) extends Tnode[T]{
     }
   }
 
+  /*
+    Find the last free index in the array, to enable to store
+    a node on that location
+   */
+  //TODO: Check hoe dit correct moet
+  private def findLastFreeIndex(): Int ={
+    val lastElement = treeArray.last
+    val lastIndex = treeArray.lastIndexOf(lastElement)
+    return lastIndex + 1
+  }
 
   private  def findFreeLocation(): Node[T]={
-    if(getRoot() == null){
+    val root = getRoot()
+    if(root == null){
       return null
-    }else{
-      //TODO: loop over de array en check alle linker en rechter $
-      // kinderen van de nodes
+
     }
 
   }
 
-
+  /*
+    This wil be used by addValue(newValue:T) to add an new value to the tree
+    the new value must be a node to be valid
+   */
   private  def addNode(newNode: Node[T]): Unit ={
     val lastNode = findFreeLocation()
-    setChild(lastNode, newNode)
-    setParent(newNode, lastNode)
+    if(lastNode == null){
+      initTree(newNode)
+    }else {
+
+      setChild(lastNode, newNode)
+      setParent(newNode, lastNode)
+      balanceTree()
+    }
     println("Node added: ", newNode)
   }
 
+  /* This wil make it possible for the user to add a value to
+      the tree
+   */
   def addValue(newValue:T): Unit={
     val newNode = new Node[T](newValue)
     println("Node created with value: ", newValue)
