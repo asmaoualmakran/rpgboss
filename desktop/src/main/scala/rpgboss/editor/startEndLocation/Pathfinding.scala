e13d2ef3e5426126767b67654dcb915e2be1f8e8
Linny123 <lindadc90@gmail.com>
2017-12-18 16:05:25 +0100

    setup random location
    Made first version for random location
    removed pathfinding

--- desktop/src/main/scala/rpgboss/editor/startEndLocation/Pathfinding.scala
+++
@@ -1,6 +0,0 @@
-package rpgboss.editor.startEndLocation
-
-class Pathfinding {
-  var length: Int = 0
-
-}
--- desktop/src/main/scala/rpgboss/editor/startEndLocation/randomLocation.scala
+++ desktop/src/main/scala/rpgboss/editor/startEndLocation/randomLocation.scala
@@ -1,6 +1,42 @@
 package rpgboss.editor.startEndLocation
 
-class randomLocation {
+
+import rpgboss.editor.{MapViewState, StateMaster}
+import rpgboss.model._
+
+class randomLocation(
+                      vs: MapViewState,
+                      totalEvents: Int,
+                      sm: StateMaster,
+                      mapname: String,
+                      maxDistance: Int,
+                      maxX: Int,
+                      maxY: Int,
+                    ) {
+  def testLocation(location: MapLoc): Unit ={
+    return true
+  }
+
+  def makeEndLocation(subLoc: Int): Unit ={
+    val dubLocX = rnd.nextInt(2 * maxDistance)
+    var loc = 1
+    if((subLoc - maxDistance) > 0 ){
+      loc = subLoc + (dubLocX - maxDistance)
+    }
+    loc
+
+
+  }
+
+
+
+  val rnd = new scala.util.Random
+  var startLocX = rnd.nextInt( maxX )
+  var startLocY = rnd.nextInt(maxY)
+  var endLocX = makeEndLocation(startLocX)
+  var endLocY = makeEndLocation(startLocY)
+
+
 
 }
 
